5a16dc5502e7652e975afe207c95e4c11d1b2048
nuritiras <nuritiras@gmail.com>
2024-03-01 10:48:28 +0300

    Proje dosyası ekle.

---
+++ OgrenciTakip/Controllers/OgrenciController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgrenciTakip.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Gonder() 
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OgrenciTakip/Models/OgrenciModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OgrenciTakip.Models
+{
+    public class OgrenciModel
+    {
+        public int OkulNo { get; set; }
+        public string? Isim { get; set; }
+        public string? SoyIsim { get; set; }
+        public string? Sinifi { get; set; }
+
+    }
+}
